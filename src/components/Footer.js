586c4df4a9e39100162a68525396ee04c744642b
Akim Ospan <ospanakim05@gmail.com>
2024-08-31 07:13:56 +0500

    Footer Component Finished

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import '../Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2024 Jana Stories. All rights reserved.</p>
+        <p>
+          Follow us on:
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"> Facebook</a> |
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"> Twitter</a> |
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"> Instagram</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
